ffea3eba3924ca1ff85060b3e2ac358fb651a50b
dimeadmin <cryptonofication>
2018-01-09 00:44:25 +0530

    fixed directory errors and typos fixed

--- share/qt/clean_mac_info_plist.py
+++ share/qt/clean_mac_info_plist.py
@@ -5,25 +5,33 @@
 
 from string import Template
 from datetime import date
+import os
 
-bitcoinDir = "./";
+bitcoin_directory =  os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+version = "unknown"
 
-inFile     = bitcoinDir+"/share/qt/Info.plist"
-outFile    = "Bitcoin-Qt.app/Contents/Info.plist"
-version    = "unknown";
 
-fileForGrabbingVersion = bitcoinDir+"bitcoin-qt.pro"
-for line in open(fileForGrabbingVersion):
-	lineArr = line.replace(" ", "").split("=");
+''' If the output folder doesn't exists? create it.'''
+output_directory = "Bitcoin-Qt.app/Contents"
+if not os.path.exists(output_directory):
+	os.makedirs(output_directory)
+
+output_file = os.path.join(output_directory,"Info.plist")
+input_file = os.path.join(bitcoin_directory,'share/qt/Info.plist')
+
+''' To grab the version number from the dimecoin-qt.pro file and store in the version variable '''
+file_for_grabbing_version = os.path.join(bitcoin_directory,"dimecoin-qt.pro")
+for line in open(file_for_grabbing_version):
+	lineArr = line.replace(" ", "").split("=")
 	if lineArr[0].startswith("VERSION"):
-		version = lineArr[1].replace("\n", "");
+		version = lineArr[1].replace("\n", "")
 
-fIn = open(inFile, "r")
-fileContent = fIn.read()
-s = Template(fileContent)
-newFileContent = s.substitute(VERSION=version,YEAR=date.today().year)
+with open(input_file,'r') as input_file:
+	file_content = input_file.read()
+	string_ = Template(file_content)
+	new_file_contents = string_.substitute(VERSION=version,YEAR=date.today().year)
 
-fOut = open(outFile, "w");
-fOut.write(newFileContent);
-
+with open(output_file,'w') as output_file:
+	output_file.write(new_file_contents)
+	
 print "Info.plist fresh created"
